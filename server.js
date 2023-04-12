9ba030b0e6c1b848a1323285192c17aa50939b64
sozankhalil <sozan.civil@gmail.com>
2023-04-13 02:11:57 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+
+const express=require('express')
+
+const port = process.env.PORT
+
+const app=express()
+
+
+app.get('/',(req,res)=>{
+    console.log(hello)
+})
+
+app.listen(port,()=>{
+    console.log(`listening on port  ${port}`)
+})
